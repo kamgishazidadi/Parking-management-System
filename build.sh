65680ac2297d71eb4a267480a98ee05d734eebb5
kamgishazidadi <kamgishazidadi@gmail.com>
2026-07-20 17:00:09 +0300

    Organize backend and frontend folders

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 set -o errexit
 
+cd backend
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
@@ -7,8 +9,3 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 
 python manage.py migrate
-
-# if [[ $CREATE_SUPERUSER ]];
-# then
-#     python manage.py createsuperuser --no-input
-# fi
